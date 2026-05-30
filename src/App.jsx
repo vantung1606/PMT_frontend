c845ca4cf2791557da1347b302d680f7cbe6ef4f
MAC AIR 2020 <macair2020@192.168.102.143>
2026-05-30 14:27:17 +0700

    chore: setup skeleton project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+	return (
+		<div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontFamily: "sans-serif" }}>
+			<h1>PMT Frontend - Skeleton</h1>
+		</div>
+	);
+}
+
+export default App;
+
